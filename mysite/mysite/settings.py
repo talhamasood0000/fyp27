400d991fa29fc4df0cf7a3c45667d513bd28dc47
talhamasood0000 <talhamasood0000@gmail.com>
2022-02-21 15:13:55 +0500

    added media and static settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import os
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
