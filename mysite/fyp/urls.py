e0a6dc7d692ace602eb8a59788a8fffc5c396c8b
talhamasood0000 <talhamasood0000@gmail.com>
2022-03-13 20:34:49 +0500

    added tags and created views

--- mysite/fyp/urls.py
+++ mysite/fyp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',views.index,name='index'),
+    path('team/',views.team,name='team'),
+    path('login/',views.login_page,name='login'),
+    path('signup/',views.signup_page,name='signup'),
 ]
--- mysite/fyp/views.py
+++ mysite/fyp/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request,'fyp/home.html')
+def index(request):
+    return render(request,'fyp/index.html')
+
+def team(request):
+    return render(request,'fyp/team.html')
+
+def login_page(request):
+    return render(request,'fyp/login.html')
+
+def signup_page(request):
+    return render(request,'fyp/signup.html')
