4acdc2cfd3ee464f84097011cbaf04890420a114
talhamasood0000 <64506978+talhamasood0000@users.noreply.github.com>
2022-04-18 12:14:01 +0500

    Changed all static files and frontend problem solved

--- mysite/static/js/main.js
+++ mysite/static/js/main.js
@@ -1,66 +1,69 @@
 (function($) {
 
-	"use strict";
+    "use strict";
 
-	var fullHeight = function() {
+    var fullHeight = function() {
 
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
+        $('.js-fullheight').css('height', $(window).height());
+        $(window).resize(function() {
+            $('.js-fullheight').css('height', $(window).height());
+        });
 
-	};
-	fullHeight();
+    };
+    fullHeight();
 
-	$('#sidebarCollapse').on('click', function () {
-      $('#sidebar').toggleClass('active');
-  });
+    $('#sidebarCollapse').on('click', function() {
+        $('#sidebar').toggleClass('active');
+    });
 
 })(jQuery);
 
 
 function openTab1() {
-	document.getElementById("tab1").style.display = "block";
-	document.getElementById("tab2").style.display = "none"; 
-	document.getElementById("tab3").style.display = "none";
-	document.getElementById("tab4").style.display = "none";
-	document.getElementById("tab5").style.display = "none";     
+    document.getElementById("tab1").style.display = "block";
+    document.getElementById("tab2").style.display = "none";
+    document.getElementById("tab3").style.display = "none";
+    document.getElementById("tab4").style.display = "none";
+    document.getElementById("tab5").style.display = "none";
 }
+
 function openTab2() {
-	document.getElementById("tab1").style.display = "none";
-	document.getElementById("tab2").style.display = "block";
-	document.getElementById("tab3").style.display = "none"; 
-	document.getElementById("tab4").style.display = "none";     
-	document.getElementById("tab5").style.display = "none";
+    document.getElementById("tab1").style.display = "none";
+    document.getElementById("tab2").style.display = "block";
+    document.getElementById("tab3").style.display = "none";
+    document.getElementById("tab4").style.display = "none";
+    document.getElementById("tab5").style.display = "none";
 }
+
 function openTab3() {
-	document.getElementById("tab1").style.display = "none";
-	document.getElementById("tab2").style.display = "none"; 
-	document.getElementById("tab3").style.display = "block"; 
-	document.getElementById("tab4").style.display = "none";
-	document.getElementById("tab5").style.display = "none";    
+    document.getElementById("tab1").style.display = "none";
+    document.getElementById("tab2").style.display = "none";
+    document.getElementById("tab3").style.display = "block";
+    document.getElementById("tab4").style.display = "none";
+    document.getElementById("tab5").style.display = "none";
 }
+
 function openTab4() {
-	document.getElementById("tab1").style.display = "none";
-	document.getElementById("tab2").style.display = "none"; 
-	document.getElementById("tab3").style.display = "none"; 
-	document.getElementById("tab4").style.display = "block";
-	document.getElementById("tab5").style.display = "none";    
+    document.getElementById("tab1").style.display = "none";
+    document.getElementById("tab2").style.display = "none";
+    document.getElementById("tab3").style.display = "none";
+    document.getElementById("tab4").style.display = "block";
+    document.getElementById("tab5").style.display = "none";
 }
+
 function openTab5() {
-	document.getElementById("tab1").style.display = "none";
-	document.getElementById("tab2").style.display = "none"; 
-	document.getElementById("tab3").style.display = "none"; 
-	document.getElementById("tab4").style.display = "none";
-	document.getElementById("tab5").style.display = "block";    
+    document.getElementById("tab1").style.display = "none";
+    document.getElementById("tab2").style.display = "none";
+    document.getElementById("tab3").style.display = "none";
+    document.getElementById("tab4").style.display = "none";
+    document.getElementById("tab5").style.display = "block";
 }
 
 
 
-$(document).ready(function(){
-	$('.chart').horizBarChart({
-		selector: '.bar',
-		speed: 3000
-	});
-	});
-
+// $(document).ready(function(){
+//	$('.chart').horizBarChart({
+//		selector: '.bar',
+//		speed: 3000
+//	});
+//	});
